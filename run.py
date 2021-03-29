2521b4e2f16f6661ebf0570807e2f553ced1ad68
ayush5harma <47772616+ayush5harma@users.noreply.github.com>
2021-03-29 18:40:54 +0530

    non vcs challenege upload

---
+++ run.py
@@ -0,0 +1,25 @@
+
+from flask import Flask
+
+"""Routes for the course resource.
+"""
+
+"""
+-------------------------------------------------------------------------
+Challenge general notes:
+-------------------------------------------------------------------------
+
+1. Bonus points for returning sensible HTTP codes from the API routes
+2. Bonus points for efficient code (e.g. title search)
+"""
+
+
+app = Flask(__name__)
+
+# Import the API routes
+from routes.course import *
+# Required because app is imported in other modules
+if __name__ == '__main__':
+    print("Loading data", end=" ")
+    print("... done")
+    app.run(debug=True)
